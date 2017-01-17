84c8813a3ca84623ee00fac3fffa5fbf5ac39674
javier muñoz <javier_munoz_navarrete@hotmail.com>
2017-01-16 21:59:23 -0300

    modifican los controlladores de devise y companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -25,6 +25,7 @@ class CompaniesController < ApplicationController
   # POST /companies.json
   def create
     @company = Company.new(company_params)
+    @company.users << current_user 
 
     respond_to do |format|
       if @company.save
@@ -69,6 +70,6 @@ class CompaniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.require(:company).permit(:nme, :address, :latitude, :longitude, :user_id)
+      params.require(:company).permit(:nme, :address, :latitude, :longitude)
     end
 end
